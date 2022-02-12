d16d5befae081ae1b48ea95c7e1f19ca3b290bd0
1ffedor <95586206+1ffedor@users.noreply.github.com>
2022-02-12 11:42:58 +0300

    5 задача

---
+++ 2_занятие_5_задача_Федоров_Ф_10Т.cpp
@@ -0,0 +1,21 @@
+
+// 2_занятие_5_задача_Федоров_Ф_10Т
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin>>a >> b;
+    if (a <= 1 and b >= 3)
+    {
+        cout << a + b;
+    }
+    else
+    {
+        cout << a - b;
+    }
+    return 0;
+}
