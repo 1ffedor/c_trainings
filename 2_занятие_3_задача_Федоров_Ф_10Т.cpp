825cbf81cfe0ee600fc1d4e12fd171bf78b8b124
1ffedor <95586206+1ffedor@users.noreply.github.com>
2022-02-12 11:39:57 +0300

    Високосный год

---
+++ 2_занятие_3_задача_Федоров_Ф_10Т.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cin>>a;
+    if (a % 4 == 0)
+    {
+        cout << "yes";
+    }
+    else
+    {
+        cout << "no";
+    }
+    return 0;
+}
